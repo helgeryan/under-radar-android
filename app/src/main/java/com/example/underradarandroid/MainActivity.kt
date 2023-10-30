ffd59eec4a4595d19be58fc1032be339fe178b8f
Ryan Helgeson <ryanhelgeson14@gmail.com>
2023-10-30 13:34:31 -0400

    Deep link the events

--- app/src/main/java/com/example/underradarandroid/MainActivity.kt
+++ app/src/main/java/com/example/underradarandroid/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.underradarandroid
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.underradarandroid.Resources.DatabaseManager.DatabaseManager
+import com.example.underradarandroid.Resources.DatabaseManager.getEventForId
 import com.example.underradarandroid.databinding.ActivityMainBinding
 
 
@@ -35,6 +38,18 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        val uri = intent.data
+        uri?.let {
+            Log.d("UR Logging", uri.toString())
+            val id = uri.getQueryParameter("id")
+            val bundle = Bundle()
+            val event = DatabaseManager.getEventForId(id!!)
+            bundle.putSerializable("event", event)
+            navController.navigate(R.id.action_navigation_home_to_eventFragment, bundle)
+            Log.d("UR Logging", "${event?.title}")
+        }
 
     }
 
