df989ff5fabcf40d39decdaf37a38aec1349d784
Ryan Helgeson <ryanhelgeson14@gmail.com>
2023-11-07 11:04:50 -0500

    part 3

--- app/src/main/java/com/example/underradarandroid/HomeFragment1.kt
+++ app/src/main/java/com/example/underradarandroid/HomeFragment1.kt
@@ -1,7 +1,21 @@
 package com.example.underradarandroid
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.underradarandroid.R
 
 class HomeFragment1 : Fragment(R.layout.fragment_home1) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button_login: Button = view.findViewById(R.id.button_login)
+        button_login.setOnClickListener {
+            val action = HomeFragment1Directions.actionHomeFragment1ToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
