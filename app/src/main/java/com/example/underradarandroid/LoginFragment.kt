b18634fe4695a1eaa266bad34b222b31284557bb
Ryan Helgeson <ryanhelgeson14@gmail.com>
2023-11-07 12:16:09 -0500

    Part 4

--- app/src/main/java/com/example/underradarandroid/LoginFragment.kt
+++ app/src/main/java/com/example/underradarandroid/LoginFragment.kt
@@ -1,6 +1,25 @@
 package com.example.underradarandroid
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button_confirm: Button = view.findViewById(R.id.button_confirm)
+        val edit_text_username: TextView = view.findViewById(R.id.edit_text_username)
+        val edit_text_password: TextView = view.findViewById(R.id.edit_text_password)
+        button_confirm.setOnClickListener {
+            val username = edit_text_username.text.toString()
+            val password = edit_text_password.text.toString()
+
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password)
+            findNavController().navigate(action)
+        }
+    }
 }
